87fbf44543d399ceb11025bee6eb010b73f72965
MOKSHITH REDDY <MOKSHITH REDDY@DESKTOP-RRK7NT6>
2019-02-15 14:56:53 +0530

    heroku changes

--- src/main/java/com/springboot/controller/HelloWorld.java
+++ src/main/java/com/springboot/controller/HelloWorld.java
@@ -9,10 +9,14 @@ public class HelloWorld {
 	@GetMapping("/hello")
 
 	public String getHelloWolrd() {
-		
+
 		return "Hello Springboot ";
 
-	
+	}
+	@GetMapping("/heroku")
+	public String getHeroku() {
+
+		return "Welcome to the Heroku world...";
 	}
 
 }
